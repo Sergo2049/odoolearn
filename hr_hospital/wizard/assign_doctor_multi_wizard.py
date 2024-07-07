ace8715c57add75f548ec31d2c750c0de5dc3054
Serhii Vydysh <helpme.bsp@gmail.com>
2024-07-07 20:21:58 +0300

    wizard assign doctor aded- works

--- hr_hospital/wizard/assign_doctor_multi_wizard.py
+++ hr_hospital/wizard/assign_doctor_multi_wizard.py
@@ -4,7 +4,13 @@ class AssignDoctorWizard(models.TransientModel):
     _name = 'assign.doctor.multi.wizard'
     _description = 'Assign doctor to patients'
 
-    doctor_id = fields.Many2one(comodel_name='hospital.patient', string='Doctor')
+    doctor_id = fields.Many2one(comodel_name='hospital.doctor', string='Doctor')
 
     def action_assign(self):
+        self.ensure_one()
+        active_ids = self.env.context.get('active_ids')
+        for patient_id in active_ids:
+            patient = self.env['hospital.patient'].browse(patient_id)
+            patient.personal_doctor_id = self.doctor_id.id
+
         return
