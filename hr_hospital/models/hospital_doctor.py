f28a25769140fc01d7a7dcb0f9317e7090b73602
Serhii Vydysh <helpme.bsp@gmail.com>
2024-07-14 00:11:10 +0300

    [IMP]Module4 : Staged

--- hr_hospital/models/hospital_doctor.py
+++ hr_hospital/models/hospital_doctor.py
@@ -9,22 +9,29 @@ class HospitalDoctor(models.Model):
 
     specialization = fields.Many2one(
         comodel_name='hospital.doctor.specialization')
-    is_intern = fields.Boolean(
-        required="is_intern=True"
-    )
+
+    is_intern = fields.Boolean()
+
     mentor_id = fields.Many2one(
         comodel_name='hospital.doctor',
         domain="[('is_intern', '=', False)]"
+    )
+    intern_ids = fields.One2many(
+        string='interns',
+        comodel_name='hospital.doctor',
+        inverse_name='mentor_id',
+        readonly=True
     )
 
     @api.onchange('is_intern')
     def check_mentor(self):
         if not self.is_intern:
             self.mentor_id = False
+
     @api.constrains('is_intern', 'mentor_id')
     def check_mentor_required(self):
         for res in self:
-            if res.is_intern and not self.mentor_id:
+            if res.is_intern and not res.mentor_id:
                 raise ValidationError('Intern must have a mentor')
 
     def get_diagnosis_to_approve(self):
