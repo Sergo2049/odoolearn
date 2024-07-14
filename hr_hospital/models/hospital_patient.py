38cad1cb9208104ba9dd0233ed2849d9d39aef13
Serhii Vydysh <helpme.bsp@gmail.com>
2024-07-14 15:56:26 +0300

    [IMP]Module4 : Staged

--- hr_hospital/models/hospital_patient.py
+++ hr_hospital/models/hospital_patient.py
@@ -8,11 +8,21 @@ class HospitalPatient(models.Model):
     _description = 'Hospital patient'
     _inherit = 'hospital.person'
 
-    personal_doctor_id = fields.Many2one(comodel_name='hospital.doctor')
+    personal_doctor_id = fields.Many2one('hospital.doctor')
+
     birth_date = fields.Date()
+
     age = fields.Integer(compute='_compute_age')
+
     passport_data = fields.Text()
+
     contact_person = fields.Char()
+
+    visit_ids = fields.One2many(
+        'hospital.visit',
+        'patient_id',
+        readonly=True
+    )
 
     @api.depends('birth_date')
     def _compute_age(self):
@@ -20,11 +30,21 @@ class HospitalPatient(models.Model):
             rec.age = relativedelta(fields.Date.today(), self.birth_date).years
 
     def action_visit_history(self):
-        print('Button ok')
+        self.ensure_one()
         return {
             "name": "Visit history",
             "type": "ir.actions.act_window",
             "res_model": "hospital.visit",
             "view_mode": "tree",
             "domain": f"[('patient_id', '=', {self.id})]"
+        }
+    def action_create_visit(self):
+        self.ensure_one()
+        return {
+            "name": "Patient",
+            "type": "ir.actions.act_window",
+            "res_model": "hospital.visit",
+            "view_mode": "form",
+            "target": "new",
+            "context": {"default_patient_id" : self.id}
         }
